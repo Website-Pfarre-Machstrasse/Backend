83d09084af52a9e3bf54556b844344322f7b4d9d
mrkonjic <SMG4SMG3SMG2ss>
2020-11-29 19:25:09 +0100

    Event.py hinzugefügt

---
+++ server/common/database/event.py
@@ -0,0 +1,15 @@
+from .ref import db
+from .mixins import UUIDKeyMixin, UUIDType
+
+
+class Event(UUIDKeyMixin, db.Model):
+    __tablename__ = 'event'
+    __no_marshmallow__ = True
+
+    name = db.Column(db.String(127), nullable=False)
+    details = db.Column(db.String(255), nullable=False)
+    start = db.Column(db.timestamp, nullable=False)
+    end = db.Column(db.timestamp, nullable=False)
+    author = db.Column(UUIDType, db.ForeignKey('user.id'), nullable=False)
+
+
